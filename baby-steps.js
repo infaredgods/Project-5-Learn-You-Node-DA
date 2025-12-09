d2d26d5ec1eec60079e86074ca997b5273518973
infaredgods <diegoaragonguzman@gmail.com>
2025-12-09 12:26:49 -0800

    challenge 3 complete

--- baby-steps.js
+++ baby-steps.js
@@ -1,12 +1,12 @@
-//gets command line arguments and slices off the first two elements
+// gets command line arguments and slices off the first two elements
 const args = process.argv.slice(2);
 
-//converts the arguments from strings to numbers
+// converts the arguments from strings to numbers
 const numbers = args.map(Number);
 
 let sum = 0;
 
-//loops through the numbers and adds them
+// loops through the numbers and adds them
 for (let i = 0; i < numbers.length; i++) {
     sum += numbers[i];
 }
---
+++ my-first-io.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+// read the file path from command line arguments
+const filePath = process.argv[2];
+
+// performs one synchronus filesystem operation
+const contents = fs.readFileSync(filePath, 'utf8');
+
+// count the number of newline characters 
+const newlineCount = contents.split('\n').length - 1;
+
+// prints the result
+console.log(newlineCount);
